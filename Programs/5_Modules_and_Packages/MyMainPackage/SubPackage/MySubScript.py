7d91013f3d2091618d222d877b4fa185a0e06f12
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-27 19:32:00 +0530

    Complete Modules and Packages

---
+++ Programs/5_Modules_and_Packages/MyMainPackage/SubPackage/MySubScript.py
@@ -0,0 +1,2 @@
+def sub_report():
+    print("Function in Sub Package")
---
+++ Programs/5_Modules_and_Packages/myprogram.py
@@ -0,0 +1,7 @@
+import mymodule
+from MyMainPackage import MainScript
+from MyMainPackage.SubPackage import MySubScript
+
+mymodule.fun()
+MainScript.main_report()
+MySubScript.sub_report()
