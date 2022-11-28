c5e5491cd32d8da3845f409e702a7dfa3977cb13
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-28 09:45:38 +0530

    Complete pylint

---
+++ Programs/7_Errors_and_Exception_Handling/2_pylint example/simple1.py
@@ -0,0 +1,19 @@
+'''
+Very Simple Script
+'''
+
+
+def fun():
+    '''
+    Function
+    '''
+
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+fun()
+
+# Run pylint simple1.py in terminal after cd-ing to this folder
