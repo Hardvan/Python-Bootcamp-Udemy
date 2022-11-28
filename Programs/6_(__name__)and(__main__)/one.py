ad0e1c5794774ae4046e643c3df446c96a263f3c
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-28 09:44:54 +0530

    Complete __name__ and __main__

---
+++ Programs/6_(__name__)and(__main__)/one.py
@@ -0,0 +1,15 @@
+
+# ? __name__ is a built-in variable which evaluates to the name of the current module.
+# * The value is set to "__main__" when the module is run as a script.
+# * If the module is imported, the value is set to the name of the module.
+
+def fun():
+    print("fun() in one.py")
+
+
+print("TOP LEVEL in one.py")
+
+if __name__ == "__main__":
+    print("one.py is being run directly!")
+else:
+    print(f"one.py has been imported.__name__ = {__name__}")
