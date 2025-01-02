44edb5282a5ee8e76fe9c27ddc5fc16358481f63
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2025-01-02 12:36:13 +0530

    Revise till Decorators

--- Programs/6_(__name__)and(__main__)/one.py
+++ Programs/6_(__name__)and(__main__)/one.py
@@ -12,4 +12,4 @@ print("TOP LEVEL in one.py")
 if __name__ == "__main__":
     print("one.py is being run directly!")
 else:
-    print(f"one.py has been imported.__name__ = {__name__}")
+    print(f"one.py has been imported.\n__name__ = {__name__}")
